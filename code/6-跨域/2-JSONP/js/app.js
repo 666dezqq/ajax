b1a9c3eec2780c4fd6179b355b0ea8bb0e9cc258
666dezqq <zhen666dezqq@163.com>
2023-06-06 11:01:56 +0800

    信息

---
+++ code/6-跨域/2-JSONP/js/app.js
@@ -0,0 +1,14 @@
+const data ={
+    name: 'zqq'
+};
+
+console.log(data);
+
+// //处理数据
+// function handle(data){
+//     //获取result元素
+//     const result = document.getElementById('result');
+//     result.innerHTML = data.name;
+// }
+
+handle(data);
